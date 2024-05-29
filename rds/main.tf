bef4260964e3b2e3aa5088717ac8a898b2ff8929
Mark DeCrane <mark.decrane@hashicorp.com>
2024-05-29 19:22:16 -0400

    wip

--- rds/main.tf
+++ rds/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 module "rds" {
   source       = "app.staging.terraform.io/example-org-6dff95/rds-stacks-test/aws"
-  version      = "1.0.1"
+  version      = "1.0.4"
   db_encrypted = true
   db_name      = "djsfs"
   db_username  = "dsjhfks"
