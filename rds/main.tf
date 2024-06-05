052854dd4c2cfbe502f9a2d131db4e3b58822350
Mark DeCrane <mark.decrane@hashicorp.com>
2024-06-05 15:06:24 -0400

    wip

--- rds/main.tf
+++ rds/main.tf
@@ -13,6 +13,11 @@ module "rds-stacks-test" {
   version = "1.0.4"
 }
 
+module "stack-mod" {
+  source  = "app.staging.terraform.io/example-org-6dff95/stack-mod/root"
+  version = "1.0.0"
+}
+
 module "kubeadm-token" {
   source  = "scholzj/kubeadm-token/random"
   version = "1.2.0"
