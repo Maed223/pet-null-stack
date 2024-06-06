0a36058a150521f0814cdaec8c6686429c348566
Mark DeCrane <mark.decrane@hashicorp.com>
2024-06-06 15:21:58 -0400

    wip

--- has-nested-deprecation/main.tf
+++ has-nested-deprecation/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.3.2"
-    }
-  }
-}
-
 module "stack-mod" {
   source  = "app.staging.terraform.io/example-org-6dff95/stack-mod/root"
   version = "1.0.0"
--- rds/main.tf
+++ rds/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.3.2"
-    }
-  }
-}
-
-
 module "rds-stacks-test" {
   source  = "app.staging.terraform.io/example-org-6dff95/rds-stacks-test/aws"
   version = "1.0.4"
